8d1eb7d88312fa5d7a3b95166068082371db812f
Sigit <sugiantorosigit3@gmail.com>
2024-08-12 20:43:57 +0700

    update repository

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,8 @@ import React from 'react'
 function Footer() {
   return (
     <div className='footer bg-white shadow mt-20'>
-    <div className='container mx-auto px-4 flex items-center justify-between'>
-      <p className='py-4'>&copy; Copyright by <span className='font-bold'>Sigit Sugiantoro</span></p>
-      <div className='social-footer flex items-center sm:gap-7 gap-2'>
-        <i className='ri-facebook-circle-fill text-2xl'></i>
-        <i className='ri-twitter-fill text-2xl'></i>
-        <i className='ri-youtube-fill text-2xl'></i>
-        <i className='ri-linkedin-box-fill text-2xl'></i>
-      </div>
+      <div className='container mx-auto px-4 flex md:justify-start justify-center'>
+        <p className='py-4'>&copy; Copyright by <span className='font-bold'>Sigit Sugiantoro</span></p>
       </div>
   </div>
   )
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import profilePicture from '../assets/profile-picture.png'
+import profilePicture from '../assets/profile.png'
 import { FaTiktok } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { HashLink } from 'react-router-hash-link';
 
 function Header() {
   return (
     <div className='header pb-10' id='header'>
         <div className='container mx-auto px-4'>
         <div className='hero grid md:grid-cols-2 grid-cols-1 items-center gap-20 pt-32'>
-          <div className='box md:order-1 order-2'>
-            <h1 className='lg:text-5xl/tight text-3xl font-medium mb-7 dark:text-white'>Hi, I'am Sigit Sugiantoro <span className='font-bold text-sky-400 underline'>Programmer - DevOps</span></h1>
-            <p className='text-base/8 mb-5 dark:text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consectetur semper libero, id sagittis massa efficitur sed. Aenean feugiat justo non ex condimentum, vel gravida mi rutrum.</p>
-            <div className='flex mb-5 gap-3 text-whi dark:text-white'>
-                <a href='' className='text-4xl'><FaInstagram /></a> 
+          <div className='box md:order-1 order-2 '>
+            <h1 className='lg:text-5xl/tight md:text-start text-center text-3xl font-medium mb-7 dark:text-white'>Hi, I'am Sigit Sugiantoro <span className='font-bold text-sky-400 underline'>Programmer - DevOps</span></h1>
+            <p className='md:text-start text-center text-base/8 mb-5 dark:text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consectetur semper libero, id sagittis massa efficitur sed. Aenean feugiat justo non ex condimentum, vel gravida mi rutrum.</p>
+            <div className='md:justify-normal justify-center flex mb-5 gap-3 dark:text-white'>
+                <a href='' className='sm:text-center  text-4xl'><FaInstagram /></a> 
                 <a href='' className='text-4xl'><FaTiktok /></a>
                 <a href='' className='text-4xl'><FaTwitter /></a>
                 <a href='' className='text-4xl'><FaFacebookF /></a>
             </div>
-            <a href='#' className='bg-sky-400 hover:bg-sky-500 transition-all py-1 px-4 text-white shadow rounded-full'>Tentang Saya <i className='ri-eye-line ms-1'></i></a>
+            <HashLink to='/#about' className='md:justify-normal md:w-36 justify-center flex bg-sky-400 hover:bg-sky-500 transition-all py-1 px-4 text-white shadow rounded-full'>Tentang Saya</HashLink>
           </div>
           <div className='box md:order-2 order-1'>
-            <img src={profilePicture} alt='Hero Image' className='md:w-300 w-[400px] mx-auto '/>
+            <img src={profilePicture} alt='Hero Image' className='md:w-full w-[900px] mx-auto '/>
           </div>
         </div>
         </div>
