2f45f3599973a161d0fa71b356581abdaacf4eba
Sigit <sugiantorosigit3@gmail.com>
2024-08-25 20:18:47 +0700

    penambahan course php

--- src/data/DataPortofolio.jsx
+++ src/data/DataPortofolio.jsx
@@ -1,44 +1,44 @@
-import landingHtml from '../assets/landing-pagae-html-css.png'
 import landingTailwind from '../assets/landing-pageTailwind.png'
 import portfolioTailwind from '../assets/portfolioTailwindCSS.png'
 import reactPage from '../assets/reactTailwind.png'
 import portfolio5 from '../assets/simpleDashboard.png'
+import portofolio6 from '../assets/portofolio6.jpeg'
 
 
 export const portofolioList = [
     {
         id:'1',
-        title: 'Landing Page HTML CSS',
-        image: landingHtml,
-        skill: 'html, css',
-        link: `https://github.com/Sigitttsss/Landing_Page-HTML_CSS`
-    },
-    {
-        id:'2',
         title: 'Landing Page Tailwind CSS',
         image: landingTailwind,
         skill: 'HTML, CSS, TailwindCSS',
         link: `https://github.com/Sigitttsss/Simple-Landing-Page_TailwindCSS`
     },
     {
-        id:'3',
+        id:'2',
         title: 'Portofolio Tailwind CSS',
         image: portfolioTailwind,
         skill: 'Tailwind CSS',
         link: `https://github.com/Sigitttsss/TailwindCSS`
     },
     {
-        id:'4',
+        id:'3',
         title: 'Landing Page ',
         image: reactPage,
         skill: 'React JS, Tailwind CSS',
         link: `https://github.com/Sigitttsss/ReactJS-Tailwind01`
     },
     {
-        id:'5',
+        id:'4',
         title: 'Dashboard Sederhana',
         image: portfolio5,
         skill: 'React Js, Library ANT Design',
         link: `https://github.com/Sigitttsss/dashboard-sederhana-ANT-Design`
+    },
+    {
+        id:'5',
+        title: 'Login System-PHP',
+        image: portofolio6,
+        skill: 'PHP',
+        link: `https://github.com/Sigitttsss/loginsystem-PHP`
     },
 ]
